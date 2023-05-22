c1f2603b242df74f0ed4af715661c6257f4f00fe
hanifanggawi <hanifanggawi45@gmail.com>
2023-05-22 08:07:28 +0700

    Revert "add log for debug"
    This reverts commit 9476130fed8ac911249a5e7c95ab15c3228b2d4e.

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -58,6 +58,7 @@ export function isSameDay(date1: Date, date2: Date) {
 export function padMonthlyData(dbRecords: HabitRecord[], habitId: number, monthNumber: number, year?: number): HabitRecord[] {
   const { startDate, endDate } = getDateLimits(monthNumber, year)
   let recordPointer = 0
+  console.log('DISINI initial dbRecords', dbRecords)
 
   for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
     if (dbRecords[recordPointer]?.date) {
